7280dc1236b2c4e4a36918b7021897f5ae6096b4
adefaze <adefaze@gmail.com>
2023-12-14 02:37:10 +0100

    added details page

--- lib/detail.dart
+++ lib/detail.dart
@@ -11,6 +11,16 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: const Chip(
+        padding: EdgeInsets.fromLTRB(55, 12, 55, 12),
+        label: Text(
+          'Order Now',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        backgroundColor: Colors.orange,
+      ),
       body: Stack(
         children: [
           Container(
@@ -59,7 +69,112 @@ class _DetailPageState extends State<DetailPage> {
               ),
             ),
           ),
-          
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.38,
+            left: 25,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black.withOpacity(.09)),
+              child: Column(
+                children: const [
+                  Text(
+                    '150+ reviews, from 75 friends ',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Nigerian Jollof',
+                    style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.47,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  topLeft: Radius.circular(20),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    buildItemContainer(
+                        'Vegetable',
+                        'This is a sub text, should break into next line when the text is longer',
+                        'food'),
+                    buildItemContainer(
+                        'Vegetable',
+                        'This is a sub text, should break into next line when the text is longer',
+                        'food'),
+                    buildItemContainer(
+                        'Vegetable',
+                        'This is a sub text, should break into next line when the text is longer',
+                        'food'),
+                    buildItemContainer(
+                        'Vegetable',
+                        'This is a sub text, should break into next line when the text is longer',
+                        'food'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildItemContainer(String title, String description, String image) {
+    return Container(
+      padding: const EdgeInsets.only(top: 20, left: 25, right: 45),
+      child: Row(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/$image.jpg'),
+                    fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(10)),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.grey.shade800),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // ignore: sized_box_for_whitespace
+              Container(
+                width: MediaQuery.of(context).size.width - 200,
+                child: Text(
+                  description,
+                  style: TextStyle(fontSize: 14, color: Colors.grey.shade800),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
